807c4f3cc838465233dd9f149c87f79e046cbc5a
Voinea Andrei <andrei.voinea93@gmail.com>
2017-06-09 12:29:00 +0300

    ESP8266 OTA Update <3

--- sketch_jan03a/sketch_jan03a.ino
+++ sketch_jan03a/sketch_jan03a.ino
@@ -1,8 +1,8 @@
 #include <ESP8266WiFi.h>
+#include <ArduinoOTA.h>
 #include <Wire.h>
 #include <VL53L0X.h>
 #include <Servo.h>
-
 
 WiFiServer server(1212);
 WiFiClient cl;
@@ -19,26 +19,40 @@ void setup()
   WiFi.softAP("ESPap");
   server.begin();
   Wire.begin();
+  ArduinoOTA.begin();
 
-  sensor.init();
-  sensor.setTimeout(500);
-  sensor.setMeasurementTimingBudget(20000);
-  sensor.startContinuous();
-
+  /*sensor.init();
+    sensor.setTimeout(500);
+    sensor.setMeasurementTimingBudget(20000);
+    sensor.startContinuous();
+  */
 
 }
 
 void loop()
 {
+  int i = 0;
   if (server.hasClient()) {
     if (!cl.connected()) {
       cl = server.available();
       Serial.println("New client!");
+      for (i = 0; i < 20; i++) {
+        digitalWrite(LED_BUILTIN, LOW);
+        delay(50);
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(50);
+      }
     }
   }
 
-  switch (cl.read())
-  {
+  ArduinoOTA.handle();
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+
+  /*switch (cl.read())
+    {
     case 'r':
 
       break;
@@ -51,24 +65,21 @@ void loop()
 
     default:
       break;
-  }
+    }
 
-  cl.println(sensor.readRangeContinuousMillimeters());
-  if (sensor.timeoutOccurred()) {
+    cl.println(sensor.readRangeContinuousMillimeters());
+    if (sensor.timeoutOccurred()) {
     cl.println(" TIMEOUT");
-  }
+    }
 
 
 
-  servo.write(135);
-
+    servo.write(135);
+  */
 }
 
 void initHardware()
 {
-  Serial.begin(115200);
-  Serial.println("");
-  //pinMode(A0, INPUT);
   servo.attach(D0);
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
